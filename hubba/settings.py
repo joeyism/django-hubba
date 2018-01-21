69c4f92afb4be7cac08afae54e7c61fd1c2624b1
joeyism <sham.joey@gmail.com>
2018-01-21 13:10:23 -0500

    Updated staticfiles_dir

--- hubba/settings.py
+++ hubba/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
+  os.path.join(SITE_ROOT, '../recommendations/static/'),
 )
 
 try:
