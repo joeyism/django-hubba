144ce856e36f773080bbaefbbe7a14b829046b16
joeyism <sham.joey@gmail.com>
2018-01-21 11:39:11 -0500

    Updated migrations

--- prod.sh
+++ prod.sh
@@ -7,5 +7,6 @@ wget https://s3.amazonaws.com/hubba-data-engineer-datasets/prods.csv
 wget https://s3.amazonaws.com/hubba-data-engineer-datasets/actions.csv
 wget https://s3.amazonaws.com/hubba-data-engineer-datasets/buyers.csv
 cd ..
+python3 manage.py migrate
 python3 manage.py load_data
 python3 manage.py load_recommendations
